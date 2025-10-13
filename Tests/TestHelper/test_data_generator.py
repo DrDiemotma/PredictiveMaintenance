c63fdb158925d76d776aaebf9f696b0cd7bd490d
DrDiemotma <dierck.matern@dmatern-freelancing.com>
2025-10-13 09:34:55 +0200

    Tested markov model for data simulation in the test data generator

--- Tests/TestHelper/test_data_generator.py
+++ Tests/TestHelper/test_data_generator.py
@@ -1,5 +1,7 @@
 import pytest
-from Tests.Helper import data_generator
+import numpy as np
+from Tests.Helper import data_generator, MarkovModelGenerator
+
 
 @pytest.mark.parametrize("sequence_length,dimension,batch_size,sequence_count", [
     (10, 5, 15, 20),
@@ -15,3 +17,18 @@ def test_data_generator(sequence_length: int, dimension: int, batch_size: int, s
         n_batches += 1
 
     assert n_batches == sequence_count
+
+
+@pytest.mark.parametrize("sequence_length,dimension,batch_size,sequence_count", [
+    (10, 5, 15, 20),
+    (11, 6, 16, 21)
+])
+def test_data_generator_markov_integration(sequence_length: int, dimension: int, batch_size: int, sequence_count: int):
+    transition_matrix: np.typing.NDArray = np.array([[0.8, 0.2, 0.0],
+                                                     [0.1, 0.8, 0.1],
+                                                     [0.3, 0.0, 0.7]],
+                                                    dtype=np.float32)
+    init_state: int = 0
+    mkg: MarkovModelGenerator = MarkovModelGenerator(transition_matrix, init_state)
+    data = data_generator(sequence_length, dimension, batch_size, sequence_count, model=mkg.get_next)
+    assert data is not None
